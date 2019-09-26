bcefa0638b6c22aa20166ee62e85c1709ad9c350
Charlie Levine <charlielevine@Charlies-MacBook-Pro.local>
2019-09-26 18:49:50 -0400

    Node es6 helloworld working

--- dist/functions/src/index.ts
+++ dist/functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/api/helloworld', (req, res) => {
+    res.json({
+        message: 'hello world'
+    });
+});
+
+exports.app = functions.https.onRequest(app);
