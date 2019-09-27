0e3ab4ef9e39aeb651c3bd384bc551efe8ed2f76
Charlie Levine <charlielevine@Charlies-MacBook-Pro.local>
2019-09-26 20:32:58 -0400

    contact service guideline

--- src/app/services/contact-manager.service.ts
+++ src/app/services/contact-manager.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginInfo, RegisterInfo } from '../layout/login/login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactManagerService {
 
-  constructor() { }
+  url = 'https://knightscontacts.com';
+
+  constructor(private http: HttpClient) { }
+
+  endpoint1(loginInfo: LoginInfo) {
+    return this.http.put(`${this.url}/api/user/login`, loginInfo);
+  }
+
+  endpoint2(registerInfo: RegisterInfo) {
+    return this.http.put(`${this.url}/api/user`, registerInfo);
+  }
+
+  endpoint3() {
+    return this.http.get(`${this.url}/api/helloworld`);
+  }
 }
