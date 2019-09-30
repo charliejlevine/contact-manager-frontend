96ca8a00c4f49d26e3e8b208ca16a0b9334af24a
Nelson Torres <ntorressm@gmail.com>
2019-09-30 15:29:47 -0400

    Added search by name/ fixed a couple of bugs

--- dist/functions/src/index.ts
+++ dist/functions/src/index.ts
@@ -6,6 +6,10 @@ import * as uniqueValidator from 'mongoose-unique-validator';
 
 const app = express();
 
+function escapeRegex(text: string) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
 // MongoDB connection.
 const URI = "mongodb+srv://admin:password321@maincluster-bmnhf.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => {
@@ -58,18 +62,38 @@ const ContactSchema = new Schema({
 const User = mongoose.model('User', UserSchema);
 const Contact = mongoose.model('Contact', ContactSchema);
 
+app.post('/api/contact:query', (req, res) => {
+    if (req.params.query) {
+        if (!req.body.userId) {
+            res.status(500).json({message: "User needed to search"});
+        }
+
+        const userId = req.body.userId;
+        const regex = new RegExp(escapeRegex(req.params.query), 'gi');
+        Contact.find({"name": regex, "userId": userId}, function (err, contacts) {
+            if (err) {
+                res.status(500).json({message: "Unknown error trying to search"});
+            } else {
+                res.json({message: "Successfully searched for query", contacts: contacts});
+            }
+        });
+    } else {
+        res.status(500).json({message: "Query needed to search contact"});
+    }
+});
+
 // Edit contact.
 app.patch('/api/contact', (req, res) => {
     const contactId = req.body.contactId;
 
-    if (typeof contactId === 'undefined') {
-		res.json({message: "Contact ID needed to edit contact.", contact: ""});
+    if (!contactId) {
+		res.send(500).json({message: "Contact ID needed to edit contact.", contact: ""});
 	} else {
         Contact.findById(contactId, function (err, contact) {
             if (err) {
-                res.json({message: "Could not find contact.", contact: ""})
+                res.send(500).json({message: "Could not find contact.", contact: ""})
             } else {
-                let info = {
+                const info = {
                     name: (contact as any).name,
                     phone: (contact as any).phone,
                     email: (contact as any).email,
@@ -77,29 +101,29 @@ app.patch('/api/contact', (req, res) => {
                     notes: (contact as any).notes
                 }
                 
-                if (req.body.name !== 'undefined') {
+                if (req.body.name) {
                     info.name = req.body.name;
                 }
 
-                if (req.body.phone !== 'undefined') {
+                if (req.body.phone) {
                     info.phone = req.body.phone;
                 }
 
-                if (req.body.email !== 'undefined') {
+                if (req.body.email) {
                     info.email = req.body.email;
                 }
 
-                if (req.body.address !== 'undefined') {
+                if (req.body.address) {
                     info.address = req.body.address;
                 }
 
-                if (req.body.notes !== 'undefined') {
+                if (req.body.notes) {
                     info.notes = req.body.notes;
                 }
 
                 Contact.findByIdAndUpdate(contactId, info, function(error, newContact) {
                     if (err) {
-                        res.json({message: "ERROR: Couldn't update contact.", contact: ""});
+                        res.send(500).json({message: "ERROR: Couldn't update contact.", contact: ""});
                     } else {
                         res.json({message: "Successfully updated contact", contact: newContact});
                     }
@@ -113,12 +137,12 @@ app.patch('/api/contact', (req, res) => {
 app.delete('/api/contact', (req, res) => {
     const contactId = req.body.contactId;
 
-	if (typeof contactId === 'undefined') {
-		res.json({message: "Contact ID needed to delete contact."});
+	if (!contactId) {
+		res.send(500).json({message: "Contact ID needed to delete contact."});
 	} else {
 		Contact.findByIdAndDelete(contactId, function (err, contact) {
 			if (err) {
-				res.json({message: "Error trying to delete contact."});
+				res.send(500).json({message: "Error trying to delete contact."});
 			} else {
 				res.json({message: "Sucessfully deleted contact."});
 			}
@@ -130,13 +154,13 @@ app.delete('/api/contact', (req, res) => {
 app.post('/api/contact', (req, res) => {
     const userId = req.body.userId;
 
-	if (typeof userId === 'undefined') {
-		res.json({message: "ID needed to get contacts.", contacts: ""});
+	if (!userId) {
+		res.send(500).json({message: "ID needed to get contacts.", contacts: ""});
 	} else {
 		Contact.find({userId: userId}).then((contacts) => {
 			res.json({messsage: "Getting all contacts successfully.", contacts: contacts});
 		}).catch(err => {
-			res.json({message: "Error trying to get all the contacts.", contacts: ""});
+			res.send(500).json({message: "Error trying to get all the contacts.", contacts: ""});
 		});
 	}
 });
@@ -145,14 +169,14 @@ app.post('/api/contact', (req, res) => {
 app.put('/api/contact', (req, res) => {
     const userId = req.body.userId;
 
-	if (typeof userId === 'undefined') {
-		res.json({message: "ID needed to add contact.", contact: ""});
+	if (!userId) {
+		res.send(500).json({message: "ID needed to add contact.", contact: ""});
 	} else {
 		const info = [req.body.name, req.body.phone, 
 			req.body.email, req.body.address, req.body.notes];
 		
 		for (let i = 0; i < info.length; i++) {
-			if (typeof info[i] === 'undefined')
+			if (!info[i])
 				info[i] = "";
 		}
 
@@ -168,9 +192,7 @@ app.put('/api/contact', (req, res) => {
 
 		newContact.save().catch(err => {
             if (err) {
-				res.json({message: "Unknown error", contact: ""});
-			} else {
-				res.json({message: "Successfully added user.", contact: newContact});
+				res.send(500).json({message: "Unknown error", contact: ""});
 			}
         });
         
@@ -183,19 +205,19 @@ app.post('/api/user', (req, res) => {
     const username = req.body.username;
 	const password = req.body.password;
 
-	if (typeof username === 'undefined') {
-		res.json({message: "Username required to login.", id: ""});
-	} else if (typeof password === 'undefined') {
-		res.json({message: "Password required to login.", id: ""});
+	if (!username) {
+		res.send(500).json({message: "Username required to login.", id: ""});
+	} else if (!password) {
+		res.send(500).json({message: "Password required to login.", id: ""});
 	} else {
 		User.findOne({'username' : username}, function (err, user: any) {
 			if (err) {
-				res.json({message: "Username does not exist", id: ""});
+				res.send(500).json({message: "Username does not exist", id: ""});
 			} else {
-				if (user.password == password) {
+				if (user.password === password) {
 					res.json({message: "Logged in", id: user._id});
 				} else {
-					res.json({message: "Incorrect password", id: ""});
+					res.send(500).json({message: "Incorrect password", id: ""});
 				}
 			}
 		});
@@ -204,13 +226,32 @@ app.post('/api/user', (req, res) => {
 
 // Register new user.
 app.put('/api/user', (req, res) => {
-    let newUser = new User({
+    if (req.body.username) {
+        User.findOne({'username' : req.body.username}, function (err, user: any) {
+            if (user) {
+                res.status(500).json({message: "Username already exists"});
+            }
+		});
+    } else {
+        res.status(500).json({message: "Username needed to create user."});
+    }
+
+    if (req.body.email) {
+        User.findOne({'email' : req.body.email}, function (err, user: any) {
+			if (user) {
+                res.status(500).send({message: "Email already exists"});
+            }
+		});
+    } else {
+        res.status(500).json({message: "Email needed to create user."});
+    }
+
+    const newUser = new User({
 		_id: new mongoose.Types.ObjectId,
 		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password
     });
-    // res.json({message: "test"});
 
 	newUser.save().then(user => {
         res.json({
